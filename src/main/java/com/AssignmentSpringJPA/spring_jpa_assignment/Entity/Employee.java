29baf82fa6a5bbcded7f486f91763f3fb87e330e
anant-ttn <anant.pandey@tothenew.com>
2025-03-20 10:57:25 +0530

    improvement in Q1: adding the setter and getters and also constructors

--- src/main/java/com/AssignmentSpringJPA/spring_jpa_assignment/Entity/Employee.java
+++ src/main/java/com/AssignmentSpringJPA/spring_jpa_assignment/Entity/Employee.java
@@ -13,4 +13,46 @@ public class Employee {
     Integer age;
     String location;
 
+    //    improvement in Q1: adding the setter and getters and also constructors
+    public Employee(Integer id, String name, Integer age, String location) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.location = location;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+
+
 }
